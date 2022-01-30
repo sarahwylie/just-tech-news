ea8868319e7d37127cc46eb67a7d88535349a190
sarahwylie <sarahannemariawylie@gmail.com>
2022-01-30 16:04:46 -0600

    user model created

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
-const { User } = require('../../models/User');
+const { User } = require('../../models/');
 
 router.get('/', (req, res) => {
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
@@ -12,21 +14,22 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-        }
-        res.json(dbUserData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    })
+  attributes: { exclude: ['password'] },
+  where: {
+    id: req.params.id
+  }
+})
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // POST /api/users
