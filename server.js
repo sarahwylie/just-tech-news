c9afe41e53e0b237b26adbe2ea5f0a81555281df
sarahwylie <sarahannemariawylie@gmail.com>
2022-02-09 13:34:35 -0600

    helpers added into handlebars

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'ivegotasecret',
   cookie: {},
